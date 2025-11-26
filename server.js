e3e2df2d7840030e48bd4677d2e474cd01089bd4
Liam O'Connor <liam.oconnor3@ucdconnect.ie>
2025-11-26 21:43:25 +0000

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import chatbotRoutes from "./routes/chatbot.js";
 import db from "./db.js";
 
-dotenv.config();
 const app = express();
 
 /* ================================
